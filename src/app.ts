c46c9a2af6f5913260b911fa7aaf193d6eaf30ee
GuilhermeTiba <gbt@cin.ufpe.br>
2021-11-25 16:48:16 -0300

    changed packagejson

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import "dotenv/config";
 import express from "express";
+const cors = require('cors')
 const registerRouter = require('../src/routes/registerRouter')
 const dashboardRouter = require('../src/routes/dashboardRouter')
 
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 app.use('', registerRouter);
 app.use('/dashboard', dashboardRouter)
--- src/routes/dashboardRouter.ts
+++ src/routes/dashboardRouter.ts
@@ -2,10 +2,11 @@ import path from "path";
 import { Router } from "express";
 import { Prisma , PrismaClient } from ".prisma/client";
 import { getBedsPercentage } from "../controllers/bedsController";
+import { authenticateToken } from "../middleware/authenticateToken";
 
 const prisma = new PrismaClient();
 const router = Router();
 
-router.get('/bedsPercentagePerStatus', getBedsPercentage)
+router.get('/bedsPercentagePerStatus',authenticateToken, getBedsPercentage)
 
 module.exports = router
