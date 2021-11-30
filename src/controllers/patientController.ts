ac7fba18a110f60c4508ea2fe063bca356cfca9a
Malu Melo <mmaalluu12@gmail.com>
2021-11-30 18:00:52 -0300

    Add changes on diagnosis model

--- src/controllers/patientController.ts
+++ src/controllers/patientController.ts
@@ -3,14 +3,15 @@ import { Prisma, PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export async function createPatient(req, res) {
-  const {sex, first_name, last_name, ssn, description, additional_informations, bed} = req.body
+  const {age, sex, first_name, last_name, ssn, diagnosis, additional_informations, bed, disease} = req.body
   const createPatient = await prisma.patient.create({
     data:{
+      age,
       sex,
       first_name,
       last_name,
       ssn,
-      description,
+      diagnosis,
       additional_informations,
       bed
     },
