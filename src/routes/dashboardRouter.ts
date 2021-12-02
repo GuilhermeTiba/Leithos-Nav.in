399e6790a76f3e1376c37b15189d939461fe0cff
GuilhermeTiba <gbt@cin.ufpe.br>
2021-12-02 18:41:15 -0300

    dashboard controllers daily/weekly/monthly/yearlyInAndOut added

--- src/routes/dashboardRouter.ts
+++ src/routes/dashboardRouter.ts
@@ -3,14 +3,22 @@ import { getBedsPercentage, getBedsQuantityPerStatus } from "../controllers/beds
 import { authenticateToken } from "../middleware/authenticateToken";
 import { getbedsStatsPerSection } from "../controllers/sectionController";
 import { getQuantityPerAge, getQuantityPerDiagnosis, getQuantityPerSex } from "../controllers/patientController";
+import { getDailyInAndOuts, getMonthlyInAndOuts, getWeeklyInAndOuts, getYearlyInAndOuts } from "../controllers/historicController";
 
 const router = Router();
 
 router.get('/bedsPercentagePerStatus',authenticateToken, getBedsPercentage);
+
 router.get('/bedsQuantityPerStatus', authenticateToken, getBedsQuantityPerStatus);
+
 router.get('/patientQuantityPerDiagnosis', authenticateToken, getQuantityPerDiagnosis)
 router.get('/patientQuantityPerSex', authenticateToken, getQuantityPerSex)
 router.get('/patientQuantityPerAge', authenticateToken, getQuantityPerAge)
+
+router.get('/dailyInAndOuts', authenticateToken, getDailyInAndOuts)
+router.get('/weeklyInAndOuts', authenticateToken, getWeeklyInAndOuts)
+router.get('/monthlyInAndOuts', authenticateToken, getMonthlyInAndOuts)
+router.get('/yearlyInAndOuts', authenticateToken, getYearlyInAndOuts)
 
 router.post('/bedsQuantityPerStatusAndSection', authenticateToken, getbedsStatsPerSection)
 
